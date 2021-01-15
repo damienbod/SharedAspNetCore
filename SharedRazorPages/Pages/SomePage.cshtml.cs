8473f452a8b59969adb48240169cf397d2d81e85
Damien <damien_bod@hotmail.com>
2021-01-15 10:25:54 +0100

    init razor pages

---
+++ SharedRazorPages/Pages/SomePage.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharedRazorPages.Pages
+{
+    public class SomePageModel : PageModel
+    {
+        private readonly ILogger<SomePageModel> _logger;
+
+        public SomePageModel(ILogger<SomePageModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
