6d5df3850423436975bba5a6d755f14acde7f15f
Damien <damien_bod@hotmail.com>
2021-01-15 08:55:26 +0100

    init project

---
+++ SharedApi/Controllers/SharedApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharedApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SharedApiController : ControllerBase
+    {
+        private readonly ILogger<SharedApiController> _logger;
+
+        public SharedApiController(ILogger<SharedApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        { 
+            return Ok(new List<string> { "data from shared api", "more data" });
+        }
+    }
+}
